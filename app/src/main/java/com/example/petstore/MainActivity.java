96c977ac89a259319519bb82856fc0dc3d63c269
Android Studio <ktoto.tam2005@mail.ru>
2023-02-06 13:46:34 +0900

    переосмысление списков

--- app/src/main/java/com/example/petstore/MainActivity.java
+++ app/src/main/java/com/example/petstore/MainActivity.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.List;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -18,18 +21,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void onClick(View view) {
         PetAPI petAPI = PetAPI.retrofit.create(PetAPI.class);
 
-        final Call<List<Pet>> call = petAPI.getPet(1023);
+        final Call<List<Pet>> call = petAPI.getPet(1102);
 
         call.enqueue(new Callback<List<Pet>>() {
             @Override
             public void onResponse(Call<List<Pet>> call, Response<List<Pet>> response) {
-                final TextView petname = (TextView) findViewById(R.id.petName);
-                petname.setText(response.body().toString());
+                if (response.isSuccessful()) {
+                    final TextView petname = (TextView) findViewById(R.id.petName);
+                    petname.setText(response.body().toString());
+                } else {
+                    ResponseBody errorBody = response.errorBody();
+                    try {
+                        Toast.makeText(MainActivity.this, errorBody.string(),
+                                Toast.LENGTH_SHORT).show();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
 
             @Override
--- app/src/main/java/com/example/petstore/PetAdapter.java
+++ app/src/main/java/com/example/petstore/PetAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,43 +15,9 @@ import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
-public class PetAdapter extends RecyclerView.Adapter<PetAdapter.Viewholder> {
+public class PetAdapter {
 
     private final static String photoUrls = "petstore.swagger.io/v2/pet/1023";
     private List<Pet> pets;
     private Context context;
-
-    PetAdapter(List<Pet> pets) { this.pets = pets; }
-
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        context = parent.getContext();
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.activity_main, parent, false);
-        return new ViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull Viewholder holder, int position) {
-        Pet pet = pets.get(position);
-    }
-
-    @Override
-    public int getItemCount() {
-        if (pets == null) {
-            return 0;
-        }
-        return pets.size();
-    }
-
-    class ViewHolder extends RecyclerView.ViewHolder {
-        TextView petName;
-        ImageView petImage;
-
-        ViewHolder(View itemView) {
-            super(itemView);
-            petName = (TextView) itemView.findViewById(R.id.petName);
-            petImage = (ImageView) itemView.findViewById(R.id.petImage);
-        }
-    }
 }
